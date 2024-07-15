a00e01376a5c8b38291ab5a75d5582619d058c79
2220280 <2220280.ecc.ac.jp>
2024-07-15 17:31:32 +0900

    ProductRecycleAdapter.ktで画像を設定するために終始しました。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,12 +48,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    //追加
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
-
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")            // 追加
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")                // 追加（画像の管理）
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
