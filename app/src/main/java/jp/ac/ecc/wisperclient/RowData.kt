5d5e95129a6334567bba016fa1d6ac562a1348af
2220280 <2220280.ecc.ac.jp>
2024-07-15 17:35:51 +0900

    レイアウトと関連するように修正しました

--- app/src/main/java/jp/ac/ecc/wisperclient/RowData.kt
+++ app/src/main/java/jp/ac/ecc/wisperclient/RowData.kt
@@ -2,8 +2,10 @@ package jp.ac.ecc.wisperclient
 
 data class RowData(
     val userId : String,
+    val userImage : String,
     val userName : String,
-    val whisperCount : String,
+    val followText : String,
+    val followerText : String,
     val followCount : String,
     val followerCount : String
 )
